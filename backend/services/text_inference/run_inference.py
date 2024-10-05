8fe3be5185095ec761da35c023b54283dd91d132
Emanuele Fratocchi <emanuele.fratochi@noovle.com>
2024-10-05 14:26:28 +0200

    backend/fraud_prediction endpoint teste

--- backend/services/text_inference/run_inference.py
+++ backend/services/text_inference/run_inference.py
@@ -1,10 +1,10 @@
 import numpy as np
 import xgboost as xgb
 
-model_loaded = xgb.Booster()
-model_loaded.load_model("../backend/models/text_model.json")
-
 def get_prediction_score(sentence_vector):
+
+    model_loaded = xgb.Booster()
+    model_loaded.load_model("../backend/models/text_model.json")
 
     #sentence_array = np.array([sentence_vector])  # replace with your feature vector
     dvector = xgb.DMatrix(sentence_vector)
